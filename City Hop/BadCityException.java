e68ed80bfbc9d3eeb0b09d8cf91ea65b8c454bce
Chris Martin <chrismartin16cm@gmail.com>
2017-06-09 13:18:32 -0400

    Update BadCityException.java

--- City Hop/BadCityException.java
+++ City Hop/BadCityException.java
@@ -1,1 +1,13 @@
-
+package CityHop;
+public class BadCityException extends Exception{
+    public BadCityException(){
+        super();
+    }
+    
+    public BadCityException(String message){
+        super(message);
+        
+    }
+    
+    
+}
