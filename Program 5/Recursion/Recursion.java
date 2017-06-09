e01d9371088e67d45f125cfe2b3c9d54f6329c58
Chris Martin <chrismartin16cm@gmail.com>
2017-06-09 13:40:28 -0400

    Update Recursion.java

--- Program 5/Recursion/Recursion.java
+++ Program 5/Recursion/Recursion.java
@@ -1,1 +1,21 @@
-
+public class Recursion{
+    public static void main(String[] args) {
+        System.out.println(f(3));
+        
+        
+        
+    }
+    public static int f(int count){
+        if(count == 1)
+            return 2;
+        else if(count == 2)
+            return -1;
+        else
+            return -1*f(count-1)+ 6*f(count-2);
+        
+    }
+    
+    
+    
+    
+}
